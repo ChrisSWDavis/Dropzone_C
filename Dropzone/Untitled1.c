bada8ed7f6e1943a056cb63f119362150a57ee8f
ChrisSWDavis <C.Davis.2@warwick.ac.uk>
2016-08-20 15:27:30 +0100

    Added looping over N dice, still buggy

--- Dropzone/Untitled1.c
+++ Dropzone/Untitled1.c
@@ -8,6 +8,8 @@ int armour = 9;
 
 int N = 1;
 
+int D = 6;
+
 // function to find the necessary minimum roll needed for a given armour and energy
 int rollneeded()
 {
@@ -33,10 +35,46 @@ int DamageFromDice(int roll)
     }
 }
 
+int storecombos(int *array, int iteration, int damage)
+{
+    int i;
+    if(iteration<=N)
+    {
+        for(i = 1;i<=D;i++)
+        {
+            damage = DamageFromDice(i) + damage;
+            storecombos(array, iteration+1,damage);
+        }
+    }
+    else
+    {
+        array[damage]++;
+    }
+    return 0;
+}
+
+int countcombos(int *array)
+{
+    int iteration = 1;
+    storecombos(array, iteration, 0);
+
+    return 0;
+}
+
 int main()
 {
 
-    // /*test function*/ printf("damage is %d\n", DamageFromDice(3));
+     /*test function*/ printf("damage is %d\n", DamageFromDice(6));
+
+    int *array;
+    array = (int *)calloc((N*2+1), sizeof(int));
+    countcombos(array);
+    int i;
+    for(i=0;i<=N*2;i++)
+    {
+        printf("damage from %d = %d\n", i, DamageFromDice(i));
+        printf("number of %ds = %d\n", i, array[i]);
+    }
 
     return 0;
 }
