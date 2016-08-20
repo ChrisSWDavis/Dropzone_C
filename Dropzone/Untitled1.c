197b58bac9dead8322b00fb8efe09a386322afa2
ChrisSWDavis <C.Davis.2@warwick.ac.uk>
2016-08-20 14:55:48 +0100

    Added functions for determining damage from one dice roll

--- Dropzone/Untitled1.c
+++ Dropzone/Untitled1.c
@@ -8,9 +8,35 @@ int armour = 9;
 
 int N = 1;
 
+// function to find the necessary minimum roll needed for a given armour and energy
+int rollneeded()
+{
+    return armour - energy + 5;
+}
+
+int DamageFromDice(int roll)
+{
+    if(rollneeded()>roll)
+    {
+        return 0;
+    }
+    else if(rollneeded()<=roll)
+    {
+        if(roll>=rollneeded()+2)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
+
 int main()
 {
 
+    // /*test function*/ printf("damage is %d\n", DamageFromDice(3));
 
     return 0;
 }
